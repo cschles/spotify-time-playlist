ac5a95e0404f36789f89a0a5e60cd569da829d6b
Camille <cschlesi@ucsd.edu>
2019-02-22 15:59:39 -0800

    getRecs and getTracks implemented

--- timeOfDayPlaylist.py
+++ timeOfDayPlaylist.py
@@ -4,6 +4,14 @@ import pprint
 import datetime
 import re
 
+token = 'BQC2sQ-hIPNwZxp4HF5YoNSV1S8VqyzVkyR9ULFOOMUuwA6bHsPamgcWuRJzmMC61ltvVzPwcNwm5J6HsD69-fKT9XAGxtORYqYSM-ciWsnhmeMmoFx68no91IXNpOuX9fz97JMMwYjvCJYv7F0kn7tB0Dg80GUMByZb0WA'
+header = {
+        'Accept': 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization': 'Bearer {}'.format(token)
+    }
+regex = re.compile('[0-9].*\\b')
+
 '''//TODO: 
 def getAuthorization():
  #   requests.get('/login', function(req,res))
@@ -14,23 +22,27 @@ def getTime(header):
     time = requests.get("https://api.spotify.com/v1/me/player", headers=header)
     output = time.json() 
     unixTime = int(output["timestamp"])
-    convertedTime = datetime.datetime.fromtimestamp(float(unixTime)/1000).strftime('%H:%M:%S.%f')
-    print(convertedTime)
+    convertedTime = datetime.datetime.fromtimestamp(float(unixTime)/1000).strftime('%H')
+    return convertedTime
 
-def getTopTracks(header):
-    tracks = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&offset=5",headers=header)
-    tracks = tracks.json()
+def getTracks(output,key):
     i = 0
-    topTracks = []
-    regex = re.compile('[0-9].*\\b')
+    tracks = []
     while (True):
         try:
-            url = str(tracks["items"][i]["external_urls"])
+            url = str(output[key][i]["external_urls"])
             id = regex.search(url)
-            topTracks.append(id.group(0))
+            tracks.append(id.group(0))
             i+=1
         except IndexError:
             break
+    return tracks
+
+def getTopTracks(header):
+    tracks = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&offset=5",headers=header)
+    toParse = tracks.json()
+    i = 0
+    topTracks = getTracks(toParse,"items")
     return topTracks
 
 def getAudioFeatures(tracks,header):
@@ -39,7 +51,8 @@ def getAudioFeatures(tracks,header):
     audFeat = features.json()
     upbeat = []
     slow = []
-    for i in range(1,19):
+    energy = .00
+    for i in range(1,6):
         track = audFeat["audio_features"][i]['id']
         danceability = audFeat["audio_features"][i]['danceability']
         if (danceability) >= .50:
@@ -47,17 +60,24 @@ def getAudioFeatures(tracks,header):
         else:
             slow.append(track)
         i+=1
-    print(upbeat)
-    print(slow)
+    time = int(getTime(header))
+    if 12 < time < 19:
+        energy = .90
+        recs = getRecs(upbeat,energy)
+        upbeat += recs
+    else:
+        energy = .50
+        recs = getRecs(slow,energy)
+        slow += recs
 
 
-token = 'BQAU6YuVWC_nAxj2hyzMvNVA3clrRDUQEvIX0ES5qcMgj89KItGYuVW9NBYHOY3TqE_mja-YnM8qu5ZyXZXBMMEJ-jCoEQjyVcCvEZmZB5GQMlTN3dew2zioSlB9_bmIPMyiBry-76ci648MH0DJy369We4DO4jS8gX_YOg'
-header = {
-        'Accept': 'application/json',
-        'Content-Type' : 'application/json',
-        'Authorization': 'Bearer {}'.format(token)
-    }
-getTime(header)
+def getRecs(seedTracks,energy):
+    seed = "%2C".join(seedTracks)
+    recs = requests.get("https://api.spotify.com/v1/recommendations?seed_tracks={}&max_energy={}".format(seed,energy),headers=header)
+    toParse = recs.json()
+    recTracks = getTracks(toParse,"tracks")
+    return recTracks
+
 tracks = getTopTracks(header)
 tracks = ",".join(tracks)
 getAudioFeatures(tracks,header)
