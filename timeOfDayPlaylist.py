cc4092dd5cc975e9b6fc06bce2202a873c3dda4b
Camille <cschlesi@ucsd.edu>
2019-03-20 18:00:05 -0700

    addTracks works but code refactoring needed

--- timeOfDayPlaylist.py
+++ timeOfDayPlaylist.py
@@ -4,7 +4,7 @@ import pprint
 import datetime
 import re
 
-token = 'BQCrpsucaykrF_a05ylQsHIyQkA250993Z083W1msxBsajqUzLg6kJnnipeHovIZfa25VV1j_jFCYVZAErRBSdI2JOgWiFbaNSMDT7v1gXMeKK-YErswamcVp_hmSuvcItvcRkjcOeGFl7_ZbyjGFOv4zfzKPAwyAjtdADpZVPFP9CEAld9oZCdJjcD847QqCRsXMW448M23Z-826LVxtcJn0rE30RSZHo4RbBi0sc5P'
+token = 'BQC2aQpulh7u-TWQjIwWMGHvflnw4amDcyKUvcGjoHRslcmmoigoVgm5pgZY9EAVwoU2oi9gXxoFXKenEj9rPJtBAjzaD73FRaU2oij1Dq59ru428557n_B93vCGDx-xTo_PX6TXN_BPiYt4EnTbYl8OUGxIkvMn-KJkJv1eu7ed63WGyrt23zDKzouQikQBu4Oz09aETupUnr-QFYjVoTwg6uBts-zyQzFeq7va6lK2fA'
 header = {
         'Accept': 'application/json',
         'Content-Type' : 'application/json',
@@ -38,10 +38,22 @@ def getTracks(output,key):
             break
     return tracks
 
+def getUri(output):
+    i = 0
+    tracks = []
+    while (True):
+        try:
+            uri = str(output["tracks"][i]["uri"])
+            tracks.append(uri)
+            i+=1
+        except IndexError:
+            break
+    #print(tracks)
+    return tracks
+
 def getTopTracks(header):
     tracks = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&offset=5",headers=header)
     toParse = tracks.json()
-    i = 0
     topTracks = getTracks(toParse,"items")
     return topTracks
 
@@ -51,6 +63,7 @@ def getAudioFeatures(tracks,header):
     audFeat = features.json()
     upbeat = []
     slow = []
+    test = []
     energy = .00
     for i in range(1,6):
         track = audFeat["audio_features"][i]['id']
@@ -64,20 +77,20 @@ def getAudioFeatures(tracks,header):
     if 12 < time < 19:
         energy = .90
         recs = getRecs(upbeat,energy)
-        upbeat += recs
+        test += recs
     else:
         energy = .50
         recs = getRecs(slow,energy)
-        slow += recs
+        test += recs
     #print(upbeat)
-    return upbeat
+    return test
 
 def getRecs(seedTracks,energy):
     seed = "%2C".join(seedTracks)
     recs = requests.get("https://api.spotify.com/v1/recommendations?seed_tracks={}&max_energy={}".format(seed,energy),headers=header)
     toParse = recs.json()
     #print(recs.content)
-    recTracks = getTracks(toParse,"tracks")
+    recTracks = getUri(toParse)
     return recTracks
 
 def createPlaylist(): #TODO: The Spotify API is currently having issues, so will implement once they update
@@ -87,18 +100,30 @@ def createPlaylist(): #TODO: The Spotify API is currently having issues, so will
     "public": 'true'
     }
     r = requests.post("https://api.spotify.com/v1/playlists",data=json.dumps(info),headers=header)
-    print(r)
+    #print(r)
 
 def getPlaylist(id):
     playlist = requests.get("https://api.spotify.com/v1/playlists/{}".format(id),headers=header)
-    print(playlist.content)
+    return playlist
 
 def addTracks(playlist,trackIds):
-    tracks = "spotify:track:".join(trackIds)
+    tracks = "%2C".join(trackIds)
+    #add = requests.post('https://api.spotify.com/v1/playlists/{}/tracks?uris=spotify:track:5JkCyeCL0S3UqzlibpeDdb'.format(playlist),headers=header)
+    #print(tracks)
     add = requests.post('https://api.spotify.com/v1/playlists/{}/tracks?uris={}'.format(playlist,tracks),headers=header)
+    print(add.content)
 
-tracks = getTopTracks(header)
-tracks = ",".join(tracks)
-up = getAudioFeatures(tracks,header)
-addTracks("6CUzlkabx0XGnwb9PkbUvn",up)
+
+def assemblePlaylist(id):
+    playlist = getPlaylist(id)
+    if playlist.status_code is 404:
+        createPlaylist()
+    tracks = getTopTracks(header)
+    tracks = ",".join(tracks)
+    recTracks = getAudioFeatures(tracks,header)
+    print(recTracks)
+    addTracks(id,recTracks)
+
+#addTracks("6CUzlkabx0XGnwb9PkbUvn",up)
 #getPlaylist('6CUzlkabx0XGnwb9PkbUvn')
+assemblePlaylist('6CUzlkabx0XGnwb9PkbUvn')
