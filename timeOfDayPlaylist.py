714596c2452d2275a9c2452247f6b93cf46c56fc
Camille <cschlesi@ucsd.edu>
2019-03-27 19:40:01 -0700

    getAudioFeatures refactored

--- timeOfDayPlaylist.py
+++ timeOfDayPlaylist.py
@@ -4,13 +4,13 @@ import pprint
 import datetime
 import re
 
-token = 'BQBN_f6WYwdMJCKrc5cbiPXmL6XhMILSbrZMFAf0eCYN_UXGvTOkOzrzdZ4GfoonKenDj4ANLECyBLc32CohTCcWRNlGW5mTjw70T4DbFpjpXKYCH4V0pEOtPSuex5qXkXIEGqQ9bZa9CZp6rM6kXuOeD8nTMYhRSCB2PaSocukPleEvJVSCBcUuUABpikQemJfIVySpkAs3z2n-rakVluegoPUnPjrZc93V38Oxfq0SsQ'
+token = 'BQBxuXjxfEBVuwoDGS30uzxH-EFSddBS-vdmUBeyQllPTkGxoTKkbOOiVPE6CIfloOk_UB85qdi-uuBqZHO3_GtgScoZREzZt4j9WtGmkxkZNr2wnl7VakhANN6yxxEb0wem-2a6uL6K031iE8-aKOXvLP_uCcpN4mSzJS6lwv0lzSoW1F2AJBLiUohohoUhc4_1Wci_SMi2N3VgvyjO76Sc3VykPtK63Dr80yyBt7YLaw'
 header = {
         'Accept': 'application/json',
         'Content-Type' : 'application/json',
         'Authorization': 'Bearer {}'.format(token)
     }
-#regex = re.compile('[0-9].*\\b')
+
 regex = re.compile('(?!.*:).*')
 '''//TODO: 
 def getAuthorization():
@@ -18,7 +18,7 @@ def getAuthorization():
     scopes = 'user-read-playback-state user-top-read'
 }
 '''
-def getTime(header):
+def getTime():
     time = requests.get("https://api.spotify.com/v1/me/player", headers=header)
     output = time.json() 
     unixTime = int(output["timestamp"])
@@ -44,47 +44,38 @@ def getUri(output,key):
             break
     return tracks
 
-
-
-def getTopTracks(header):
-    tracks = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=6",headers=header)
+def getTopTracks():
+    tracks = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term",headers=header)
     toParse = tracks.json()
     topTracks = getUri(toParse,"items")
     return topTracks
 
-def getAudioFeatures(tracks,header):
-    #TODO: Go through and check when spotify:track is needed and when just the track id is needed. Probably need to rethink how this is set up
+def getAudioFeatures(energy,tracks):
     trackIDs = getIDs(tracks)
     trackIDs = ",".join(trackIDs)
     features = requests.get("https://api.spotify.com/v1/audio-features?ids='{}'".format(trackIDs),headers=header)
     audFeat = features.json()
-    upbeat = []
-    slow = []
     timely = []
-    energy = .00
-    time = int(getTime(header))
-    #if 12 < time < 19:
-    for i in range(1,5):
-        track = audFeat["audio_features"][i]['id']
-        danceability = audFeat["audio_features"][i]['danceability']
-        if (danceability) >= .50:
-            upbeat.append(track)
-        else:
-            slow.append(track)
-    #print(upbeat)
-    time = int(getTime(header))
-    if 12 < time < 19:
-        energy = .90
-        recs = getRecs(upbeat,energy)
-        timely += upbeat
-        timely += recs
+    rec = []
+    if energy > .90:
+        for i in range(1,18):
+            danceability = audFeat["audio_features"][i]['danceability']
+            if danceability >= .50:
+                track = audFeat["audio_features"][i]['uri']
+                id = audFeat["audio_features"][i]['id']
+                timely.append(track)
+                rec.append(id)
     else:
-        energy = .50
-        recs = getRecs(slow,energy)
-        timely += slow
-        timely += recs
-    #print(upbeat)
-    return timely
+        for i in range(1,18):
+            danceability = audFeat["audio_features"][i]['danceability']
+            if danceability < .50:
+                track = audFeat["audio_features"][i]['uri']
+                id = audFeat["audio_features"][i]['id']
+                timely.append(track)
+                rec.append(id)
+    recs = getRecs(rec,energy)
+    timely += recs
+    return timely    
 
 def getRecs(seedTracks,energy):
     seed = "%2C".join(seedTracks)
@@ -101,7 +92,7 @@ def createPlaylist(): #TODO: The Spotify API is currently having issues, so will
     "public": 'true'
     }
     r = requests.post("https://api.spotify.com/v1/playlists",data=json.dumps(info),headers=header)
-    #print(r)
+    print(r)
 
 def getPlaylist(id):
     playlist = requests.get("https://api.spotify.com/v1/playlists/{}".format(id),headers=header)
@@ -109,23 +100,24 @@ def getPlaylist(id):
 
 def addTracks(playlist,trackIds):
     tracks = "%2C".join(trackIds)
-    #add = requests.post('https://api.spotify.com/v1/playlists/{}/tracks?uris=spotify:track:5JkCyeCL0S3UqzlibpeDdb'.format(playlist),headers=header)
-    #print(tracks)
     add = requests.post('https://api.spotify.com/v1/playlists/{}/tracks?uris={}'.format(playlist,tracks),headers=header)
     print(add.content)
 
+def energyLevel(time):
+    if 12 < time < 19:
+        energy = .90
+    else:
+        energy = .50
+    return energy
 
 def assemblePlaylist(id):
     playlist = getPlaylist(id)
     if playlist.status_code is 404:
         createPlaylist()
-    tracks = getTopTracks(header)
-    #tracks = ",".join(tracks)
-    #print(tracks)
-    recTracks = getAudioFeatures(tracks,header)
-    #print(recTracks)
+    tracks = getTopTracks()
+    time = int(getTime())
+    energy = energyLevel(time)
+    recTracks = getAudioFeatures(energy,tracks)
     addTracks(id,recTracks)
 
-#addTracks("6CUzlkabx0XGnwb9PkbUvn",up)
-#getPlaylist('6CUzlkabx0XGnwb9PkbUvn')
 assemblePlaylist('6CUzlkabx0XGnwb9PkbUvn')
